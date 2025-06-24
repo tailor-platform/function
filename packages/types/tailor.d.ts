6d90c5307d5f854f0c2ebec3a667c11fdd36ff9a
takumiyoshikawa <happyriver157@gmail.com>
2025-06-24 18:52:09 +0900

    fix: secrete manager may return undefined

--- packages/types/tailor.d.ts
+++ packages/types/tailor.d.ts
@@ -31,8 +31,24 @@ declare const tailordb: {
 };
 
 declare namespace tailor.secretmanager {
-  // getSecrets returns multiple secret objects(key=name, value=secret) at once according to vault, names
-  function getSecrets(vault: string, names: string[]): Promise<Record<string, string>>
-  // getSecret returns a secret according to vault, name
-  function getSecret(vault: string, name: string): Promise<string>
+  /**
+   * getSecrets returns multiple secret objects (key = name, value = secret)
+   * at once according to vault and secret names.
+   * 
+   * If a secret does not exist, it will not be included in the result.
+   */
+  function getSecrets<const T extends readonly string[]>(
+    vault: string,
+    names: T
+  ): Promise<Partial<Record<T[number], string>>>;
+
+  /**
+   * getSecret returns a secret according to vault and name.
+   * 
+   * If the secret does not exist, undefined is returned.
+   */
+  function getSecret(
+    vault: string,
+    name: string
+  ): Promise<string | undefined>;
 }
